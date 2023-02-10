7e2b6227a4ec51a986aef5bc95f6a206a73edd17
isohmaohot <sarahlaurel713@gmail.com>
2023-02-11 06:21:54 +0700

    Update m-trojan.sh

--- menu/m-trojan.sh
+++ menu/m-trojan.sh
@@ -3,7 +3,7 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 CEKEXPIRED () {
     today=$(date -d +1day +%Y-%m-%d)
-    Exp1=$(curl -sS https://raw.githubusercontent.com/pontora/autoscript-multiws/main/permission/ip | grep $MYIP | awk '{print $3}')
+    Exp1=$(curl -sS https://raw.githubusercontent.com/isohmaohot/autoscript-multiws/main/permission/ip | grep $MYIP | awk '{print $3}')
     if [[ $today < $Exp1 ]]; then
     echo -e "\e[32mSTATUS SCRIPT AKTIF...\e[0m"
     else
@@ -11,7 +11,7 @@ CEKEXPIRED () {
     exit 0
 fi
 }
-IZIN=$(curl -sS https://raw.githubusercontent.com/pontora/autoscript-multiws/main/permission/ip | awk '{print $4}' | grep $MYIP)
+IZIN=$(curl -sS https://raw.githubusercontent.com/isohmaohot/autoscript-multiws/main/permission/ip | awk '{print $4}' | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
 echo -e "\e[32mPermission Accepted...\e[0m"
 CEKEXPIRED
@@ -46,5 +46,5 @@ case $opt in
 5) clear ; cek-tr ;;
 0) clear ; menu ;;
 x) exit ;;
-*) echo "SALAH TEKAN" ; sleep 1 ; m-trojan ;;
+*) echo "Masukkan Angka Yang Benar" ; sleep 1 ; m-trojan ;;
 esac
